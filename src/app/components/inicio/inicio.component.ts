4ef2f82a5ff4270d01969a37b362e6b5f70872d0
DAcajabon <diego.acajabon@gmail.com>
2018-11-26 12:29:47 -0600

    Ya tiene paginado y Funciona todo

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { ProyectoService } from 'src/app/services/proyecto.service';
 import { Proyecto } from 'src/app/model/Proyecto';
-import { query } from '@angular/core/src/render3/query';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
 
   constructor(private db: AngularFireDatabase, private service: ProyectoService) {
@@ -17,47 +17,43 @@ export class InicioComponent implements OnInit {
   proyectos: Proyecto[];
   gotKey: boolean = false;
   lastKey: string = '';
-
-  @HostListener("window:scroll", ['$event'])
-  scroll(event){
-/*
-    $event: Event   // parametro
-    const top = window.scrollY
-    const doc = document.body.offsetHeight;
-    const win = window.outerHeight;
-    if (Math.trunc(top) == (doc - win + 32)){
-      alert(this.lastKey)
-    }
-*/
-    console.log('scrolled')
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      this.more();
-    }
-  }
+  finish: boolean;
+  start: boolean;
+  pos: number = 0;
+  keyPag: string[] = [];
 
   ngOnInit() {
+    this.start = false;
     let array = [];
     this.service.getProyect(5)
     .snapshotChanges()
     .subscribe(list => {
       list.forEach(proy => {
         let x = proy.payload.toJSON();
-        x['$key'] = proy.key;
         let y = x as Proyecto;
         y.key = proy.key;
-        if (y.estado == 'Terminado'){
-          array.push(y)
-        }
-        if (!this.gotKey){
-          this.lastKey = y.key;
-          this.gotKey = true;
+        if (x == null){
+          this.finish = true
+        }else{
+          if (y.estado == 'Terminado'){
+            array.push(y)
+          }
+          if (!this.gotKey){
+            this.lastKey = y.key;
+            this.gotKey = true;
+          }
         }
       })
     });
+    this.gotKey = false;
     this.proyectos = array;
+    if (!this.keyPag.includes(this.lastKey)){
+      this.keyPag.push(this.lastKey)
+    }
   }
 
-  more(){
+  next(){
+    this.start = true;
     let array2 = [];
     let cont = 1;
     this.service.getProyect(5, this.lastKey)
@@ -68,6 +64,9 @@ export class InicioComponent implements OnInit {
         x['$key'] = proy.key;
         let y = x as Proyecto;
         y.key = proy.key;
+        if (x == null){
+          this.finish = true;
+        }else{
         if (y.estado == 'Terminado'){
           if (cont <= 5){
             array2.push(y);
@@ -76,10 +75,52 @@ export class InicioComponent implements OnInit {
             }
           }
         }
-        cont += 1;
+      }
+      cont += 1;
       })
     });
-    this.proyectos = array2.reverse();
+    this.proyectos = array2;
+    this.pos += 1;
+    if (!this.keyPag.includes(this.lastKey)){
+      this.keyPag.push(this.lastKey)
+    }
+  }
+
+  previous(){
+    this.pos -= 1;
+    if (this.pos == 0){
+      this.ngOnInit();
+    }else{
+      let array2 = [];
+      let cont = 1;
+      this.service.getProyect(5, this.keyPag[this.pos])
+      .snapshotChanges()
+      .subscribe(list => {
+        list.forEach(proy => {
+          let x = proy.payload.toJSON();
+          x['$key'] = proy.key;
+          let y = x as Proyecto;
+          y.key = proy.key;
+          if (x == null){
+            this.finish = true;
+          }else{
+          if (y.estado == 'Terminado'){
+            if (cont <= 5){
+              array2.push(y);
+              if (cont == 1){
+                this.lastKey = y.key;
+              }
+            }
+          }
+        }
+        cont += 1;
+        })
+      });
+      this.proyectos = array2;
+      if (!this.keyPag.includes(this.lastKey)){
+        this.keyPag.push(this.lastKey)
+      }  
+    }
   }
 
 }
